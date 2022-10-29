2083edc7e4db48c81182d48efb08844794f773d5
budakenes <116315357+budakenes@users.noreply.github.com>
2022-10-29 20:54:59 +0300

    Update SınıfGeçmeDurumu.java

--- Sınıf Geçme Durumu/SınıfGeçmeDurumu.java
+++ Sınıf Geçme Durumu/SınıfGeçmeDurumu.java
@@ -5,18 +5,23 @@ public class SınıfGeçmeDurumu {
         Scanner inp=new Scanner(System.in);
         int mat, fiz, tur, kim, muzik;
         double ort;
+        
         System.out.print("Matematik Notunuz : ");
         mat=inp.nextInt();
         mat = mat>0 && mat<=100 ? mat : 0;
+        
         System.out.print("Fizik Notunuz : ");
         fiz=inp.nextInt();
         fiz = fiz>0 && fiz<=100 ? fiz : 0;
+        
         System.out.print("Türkçe Notunuz : ");
         tur=inp.nextInt();
         tur = tur>0 && tur<=100 ? tur : 0;
+        
         System.out.print("Kimya Notunuz : ");
         kim=inp.nextInt();
         kim = kim>0 && kim<=100 ? kim : 0;
+        
         System.out.print("Müzik Notunuz : ");
         muzik=inp.nextInt();
         muzik = muzik>0 && muzik<=100 ? muzik : 0;
