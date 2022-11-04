87eafa1801bf4d8ea3c1d54d418bf1c9c612d5ae
budakenes <116315357+budakenes@users.noreply.github.com>
2022-11-04 23:33:18 +0300

    Add files via upload

---
+++ Girilen Sayılardan Min ve Max Değerleri Bulma/GirilenSayılarınMinMaxDeğerleriBulma.java
@@ -0,0 +1,34 @@
+package giris;
+import java.util.Scanner;
+public class GirilenSayılarınMinMaxDeğerleriBulma {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int counter, number, min = 1, max = 1;
+
+        System.out.print("Kaç tane sayı gireceksiniz : ");
+        counter = input.nextInt();
+
+        for (int i = 1; i <= counter; i++){
+            System.out.print("Sayıyı giriniz : ");
+            number = input.nextInt();
+
+            if (i == 1){
+                max = number;
+                min = number;
+            }
+
+            if (number > max){
+                max = number;
+            }
+
+            if (number < min){
+                min = number;
+            }
+        }
+
+        System.out.println("En buyuk sayi: " + max);
+        System.out.println("En kucuk sayi: " + min);
+    }
+}
+
+
